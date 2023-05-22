bddf76bea00ca634d70f9fc0e2708ce55f7ee827
BekoevaIlona <illlona.bekoeva@yandex.ru>
2023-05-22 16:52:38 +0300

    добавила все кнопки на меню

--- Train/FormMenu.Designer.cs
+++ Train/FormMenu.Designer.cs
@@ -30,10 +30,10 @@
         {
             this.buttonClose = new System.Windows.Forms.Button();
             this.buttonPlay = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.buttonLanguage = new System.Windows.Forms.Button();
+            this.buttonTopic = new System.Windows.Forms.Button();
+            this.buttonInstruction = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonClose
@@ -56,60 +56,85 @@
             // 
             // buttonPlay
             // 
+            this.buttonPlay.BackColor = System.Drawing.Color.White;
+            this.buttonPlay.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(26)))), ((int)(((byte)(12)))));
+            this.buttonPlay.FlatAppearance.BorderSize = 6;
+            this.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlay.Font = new System.Drawing.Font("a_MachinaOrtoSls", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonPlay.Location = new System.Drawing.Point(619, 140);
             this.buttonPlay.Name = "buttonPlay";
             this.buttonPlay.Size = new System.Drawing.Size(424, 66);
             this.buttonPlay.TabIndex = 3;
             this.buttonPlay.Text = "играть";
-            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // buttonLanguage
             // 
-            this.button2.Location = new System.Drawing.Point(619, 240);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(424, 66);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonLanguage.BackColor = System.Drawing.Color.White;
+            this.buttonLanguage.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(26)))), ((int)(((byte)(12)))));
+            this.buttonLanguage.FlatAppearance.BorderSize = 6;
+            this.buttonLanguage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLanguage.Font = new System.Drawing.Font("a_MachinaOrtoSls", 22.2F, System.Drawing.FontStyle.Bold);
+            this.buttonLanguage.Location = new System.Drawing.Point(619, 240);
+            this.buttonLanguage.Name = "buttonLanguage";
+            this.buttonLanguage.Size = new System.Drawing.Size(424, 66);
+            this.buttonLanguage.TabIndex = 4;
+            this.buttonLanguage.Text = "язык";
+            this.buttonLanguage.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // buttonTopic
             // 
-            this.button3.Location = new System.Drawing.Point(619, 340);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(424, 66);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonTopic.BackColor = System.Drawing.Color.White;
+            this.buttonTopic.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(26)))), ((int)(((byte)(12)))));
+            this.buttonTopic.FlatAppearance.BorderSize = 6;
+            this.buttonTopic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTopic.Font = new System.Drawing.Font("a_MachinaOrtoSls", 22.2F, System.Drawing.FontStyle.Bold);
+            this.buttonTopic.Location = new System.Drawing.Point(619, 340);
+            this.buttonTopic.Name = "buttonTopic";
+            this.buttonTopic.Size = new System.Drawing.Size(424, 66);
+            this.buttonTopic.TabIndex = 5;
+            this.buttonTopic.Text = "тематика";
+            this.buttonTopic.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // buttonInstruction
             // 
-            this.button4.Location = new System.Drawing.Point(619, 440);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(424, 66);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonInstruction.BackColor = System.Drawing.Color.White;
+            this.buttonInstruction.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(26)))), ((int)(((byte)(12)))));
+            this.buttonInstruction.FlatAppearance.BorderSize = 6;
+            this.buttonInstruction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInstruction.Font = new System.Drawing.Font("a_MachinaOrtoSls", 22.2F, System.Drawing.FontStyle.Bold);
+            this.buttonInstruction.Location = new System.Drawing.Point(619, 440);
+            this.buttonInstruction.Name = "buttonInstruction";
+            this.buttonInstruction.Size = new System.Drawing.Size(424, 66);
+            this.buttonInstruction.TabIndex = 6;
+            this.buttonInstruction.Text = "правила";
+            this.buttonInstruction.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // buttonExit
             // 
-            this.button5.Location = new System.Drawing.Point(619, 540);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(424, 66);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.buttonExit.BackColor = System.Drawing.Color.White;
+            this.buttonExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(26)))), ((int)(((byte)(12)))));
+            this.buttonExit.FlatAppearance.BorderSize = 6;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("a_MachinaOrtoSls", 22.2F, System.Drawing.FontStyle.Bold);
+            this.buttonExit.Location = new System.Drawing.Point(619, 540);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(424, 66);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "выход";
+            this.buttonExit.UseVisualStyleBackColor = false;
             // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Train.Properties.Resources.форма2_меню1;
+            this.BackgroundImage = global::Train.Properties.Resources.форма2_меню2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1567, 763);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonInstruction);
+            this.Controls.Add(this.buttonTopic);
+            this.Controls.Add(this.buttonLanguage);
             this.Controls.Add(this.buttonPlay);
             this.Controls.Add(this.buttonClose);
             this.DoubleBuffered = true;
@@ -125,9 +150,9 @@
         #endregion
         private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.Button buttonPlay;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button buttonLanguage;
+        private System.Windows.Forms.Button buttonTopic;
+        private System.Windows.Forms.Button buttonInstruction;
+        private System.Windows.Forms.Button buttonExit;
     }
 }
--- Train/Properties/Resources.Designer.cs
+++ Train/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace Train.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap форма2_меню2 {
+            get {
+                object obj = ResourceManager.GetObject("форма2_меню2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
